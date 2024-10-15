9277237867058d55ce925a5a950fa641d4454513
Ou <orongxing@gmail.com>
2024-10-16 01:02:29 +0800

    feat: x-nitro-go

--- src/runtime/internal/route-rules.ts
+++ src/runtime/internal/route-rules.ts
@@ -4,6 +4,7 @@ import {
   appendResponseHeader,
   eventHandler,
   getHeader,
+  getRequestURL,
   proxyRequest,
   sendRedirect,
   setHeaders,
@@ -18,11 +19,12 @@ const _routeRulesMatcher = toRouteMatcher(
   createRadixRouter({ routes: config.nitro.routeRules })
 );
 
-const redirectedHeader = "x-nitro-redirect"
-const proxiedHeader = "x-nitro-proxy"
+// don't redirect and proxy
+const goHeader = "x-nitro-go"
 export function createRouteRulesHandler(ctx: {
   localFetch: typeof globalThis.fetch;
 }) {
+  // Will merge all rules
   return eventHandler((event) => {
     // Match route options against path
     const routeRules = getRouteRules(event);
@@ -30,46 +32,48 @@ export function createRouteRulesHandler(ctx: {
     if (routeRules.headers) {
       setHeaders(event, routeRules.headers);
     }
-    const isRedirected = !!getHeader(event, redirectedHeader)
-    const isProxied = !!getHeader(event, proxiedHeader)
-    // Apply redirect options
-    if (!isRedirected && routeRules.redirect) {
-      let target = routeRules.redirect.to;
-      if (target.endsWith("/**")) {
-        let targetPath = event.path;
-        const strpBase = (routeRules.redirect as any)._redirectStripBase;
-        if (strpBase) {
-          targetPath = withoutBase(targetPath, strpBase);
+    const isFirm = !!routeRules.headers?.[goHeader] || getHeader(event, goHeader)
+    console.log(getRequestURL(event).pathname, routeRules, isFirm)
+    if (!isFirm) {
+      // Apply redirect options
+      if (routeRules.redirect) {
+        let target = routeRules.redirect.to;
+        if (target.endsWith("/**")) {
+          let targetPath = event.path;
+          const strpBase = (routeRules.redirect as any)._redirectStripBase;
+          if (strpBase) {
+            targetPath = withoutBase(targetPath, strpBase);
+          }
+          target = joinURL(target.slice(0, -3), targetPath);
+        } else if (event.path.includes("?")) {
+          const query = getQuery(event.path);
+          target = withQuery(target, query);
         }
-        target = joinURL(target.slice(0, -3), targetPath);
-      } else if (event.path.includes("?")) {
-        const query = getQuery(event.path);
-        target = withQuery(target, query);
+        appendResponseHeader(event, goHeader, "true");
+        return sendRedirect(event, target, routeRules.redirect.statusCode);
       }
-      appendResponseHeader(event, redirectedHeader, "true");
-      return sendRedirect(event, target, routeRules.redirect.statusCode);
-    }
-    // Apply proxy options
-    if (!isProxied && routeRules.proxy) {
-      let target = routeRules.proxy.to;
-      if (target.endsWith("/**")) {
-        let targetPath = event.path;
-        const strpBase = (routeRules.proxy as any)._proxyStripBase;
-        if (strpBase) {
-          targetPath = withoutBase(targetPath, strpBase);
+      // Apply proxy options
+      if (routeRules.proxy) {
+        let target = routeRules.proxy.to;
+        if (target.endsWith("/**")) {
+          let targetPath = event.path;
+          const strpBase = (routeRules.proxy as any)._proxyStripBase;
+          if (strpBase) {
+            targetPath = withoutBase(targetPath, strpBase);
+          }
+          target = joinURL(target.slice(0, -3), targetPath);
+        } else if (event.path.includes("?")) {
+          const query = getQuery(event.path);
+          target = withQuery(target, query);
         }
-        target = joinURL(target.slice(0, -3), targetPath);
-      } else if (event.path.includes("?")) {
-        const query = getQuery(event.path);
-        target = withQuery(target, query);
+        return proxyRequest(event, target, {
+          fetch: ctx.localFetch,
+          headers: {
+            [goHeader]: "true",
+          },
+          ...routeRules.proxy,
+        });
       }
-      return proxyRequest(event, target, {
-        fetch: ctx.localFetch,
-        headers: {
-          [proxiedHeader]: "true",
-        },
-        ...routeRules.proxy,
-      });
     }
   });
 }
